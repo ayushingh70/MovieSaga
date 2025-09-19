d938a920f12ffe1f9de71c1b51b275e844d74cbd
Ayush <aniketom70@gmail.com>
2025-09-19 18:21:58 +0530

    Updated

---
+++ app/src/main/java/com/android/moviesaga/designs/ShimmerBox.kt
@@ -0,0 +1,38 @@
+package com.android.moviesaga.designs
+
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.getValue
+
+
+@Composable
+fun ShimmerBox(modifier: Modifier = Modifier) {
+    val infiniteTransition = rememberInfiniteTransition(label = "")
+    val alpha by infiniteTransition.animateFloat(
+        initialValue = 0.3f,
+        targetValue = 1f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1000, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        ),
+        label = ""
+    )
+
+    Box(
+        modifier = modifier
+            .alpha(alpha)
+            .background(Color.DarkGray, RoundedCornerShape(10.dp))
+    )
+}
